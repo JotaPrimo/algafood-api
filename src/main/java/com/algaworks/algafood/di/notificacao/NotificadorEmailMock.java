2ecbc9f5a35af228b2136f25ef9240c1e7935797
Jota Santos <jailsonsantosdpge@outlook.com>
2024-09-20 12:47:11 -0400

    Mudando o comportamento da aplicação com Spring Profiles

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -4,21 +4,18 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-// essa anotação faz com que o spring gerencia estado e injeta esse componente
-// instancia e configura
-
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-    public NotificadorEmail() {
-        System.out.println("PROD : NotificadorEmail");
+    public NotificadorEmailMock() {
+        System.out.println("DEV : NotificadorEmailMock");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s : %s\n",
+        System.out.printf("MOCK: Notificando %s através do e-mail %s : %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,20 +1,20 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
+
+    @TipoDoNotificador(NivelUrgencia.URGENTE)
     @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("AtivacaoClienteService " + notificador);
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
